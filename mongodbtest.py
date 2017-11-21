f98942300b93d488648991bcaf8af88c0d6886bc
Alex Eladas <alexeladas@gmail.com>
2017-11-21 19:51:24 +0000

    modification for tests

--- mongodbtest.py
+++ mongodbtest.py
@@ -6,40 +6,40 @@ from  flask_jsonpify import jsonify
 from google.cloud import vision
 from google.cloud.vision import types
 import io
-
-client = pymongo.MongoClient('35.196.76.140',27017,connect=False)
+from werkzeug.utils import secure_filename
+import os
+dbclient = pymongo.MongoClient('35.196.76.140',27017,connect=False)
 application = Flask(__name__)
 api = Api(application)
+UPLOAD_FOLDER = '/opt/app/SpenTrack/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+application.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 class SpenTrack(Resource):
     def get(self):
-        print("sup")
+        return("sup")
 
     def post(self):
-
-        data = request.get_data()
+        print("supp")
+        file = request.files['media']
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(application.config['UPLOAD_FOLDER'], filename))
         client = vision.ImageAnnotatorClient()
-
-        with io.open(data, 'rb') as image_file:
+        print(filename)
+        with io.open('uploads/' + filename, 'rb') as image_file:
             content = image_file.read()
 
         image = types.Image(content=content)
 
         response = client.text_detection(image=image)
         texts = response.text_annotations
-
         print(texts)
-
-
-        data = request.get_json()
-        db = client['test']
+        db = dbclient['test']
         collection = db['test_collection']
-        test = {"name": data['name'],
-            "sup": data['sup']}
-        id = collection.insert_one(test).inserted_id
         v = { str(x['_id']):x for x in collection.find()}
         print (v)
-        return "hey"
+        return {'value': texts}
 
 api.add_resource(SpenTrack, '/spentrack') # Route_1
 
